94c1f53adb5a83c04eeefb83a6d0302b54d657cc
Leuquary <raquelmoreira847@gmail.com>
2026-04-27 19:07:13 -0300

    lendo o arquivo ClientesBanco.csv via pandas

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 def main():
-    print("Hello, World!")
+    tabela_clientes = pd.read_csv("ClientesBanco.csv", encoding="latin-1")
+    print(tabela_clientes)
 
 if __name__ == "__main__": 
     main()

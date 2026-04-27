e20e547397846cae977050ee2010676d5a291b9f
Leuquary <raquelmoreira847@gmail.com>
2026-04-27 19:08:56 -0300

    removendo células nulas do arquivo csv

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ def main():
     tabela_clientes = pd.read_csv("ClientesBanco.csv", encoding="latin-1")
     print(tabela_clientes)
 
+    tabela_clientes = tabela_clientes.dropna()
+    print(tabela_clientes.info())
+    print(tabela_clientes.describe().round(1))
+
 if __name__ == "__main__": 
     main()
